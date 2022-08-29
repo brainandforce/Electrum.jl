9d7dc3066d0f5fd0692f146031bf79886be7bf97
Brandon Flores <bsflores@wisc.edu>
2022-08-29 13:32:01 -0500

    Refactored code to work with `AtomList` rather than `AbstractCrystal`

--- src/software/lammps.jl
+++ src/software/lammps.jl
@@ -1,8 +1,9 @@
 """
     write_lammps_data(
         io::IO,
-        xtal::Crystal{D};
-        supercell = diagm(ones(Int, D))::AbstractVector{<:Integer}
+        xtal::AbstractCrystal{D};
+        supercell = diagm(ones(Int, D))::AbstractVector{<:Integer};
+        dummy::Bool = false 
     )
 
 Writes crystal information to a LAMMPS data format that can be used to define a simulation box
@@ -12,18 +13,18 @@ This function currently only works for 3D systems.
 """
 function write_lammps_data(
     io::IO,
-    xtal::AbstractCrystal{D},
+    list::AtomList{D},
     supercell::AbstractMatrix{<:Integer};
     dummy::Bool=false
 ) where D
     println(io, "# LAMMPS data file written by Xtal.jl (https://github.com/brainandforce/Xtal.jl)")
     # Get the number of atoms; write the corresponding line
-    natoms = natom_cell(xtal) * convert(Int, abs(det(supercell)))
+    natoms = size(list) * convert(Int, abs(det(supercell)))
     println(io, natoms, " atoms")
     # Get the number of atom types
-    println(io, natomtypes(xtal), " atom types")
+    println(io, natomtypes(list), " atom types")
     # Convert to upper triangular form
-    bnew = triangularize(basis(xtal, primitive=true), supercell)
+    bnew = triangularize(basis(list), supercell)
     # Now print the new basis vectors
     println(io, @sprintf("0.000000    %f", bnew[1,1]), "     xlo xhi")
     println(io, @sprintf("0.000000    %f", bnew[2,2]), "     ylo yhi")
@@ -33,30 +34,46 @@ function write_lammps_data(
         println(io, @sprintf("%f %f %f", bnew[1,2], bnew[1,3], bnew[2,3]), " xy xz yz")
     end
     # Get the different atom types
-    # First enumerate the atoms - by name, not atomic number
-    atl = (dummy ? xtal.gen : remove_dummies(xtal.gen))
+    # Strip dummy atoms if needed
+    atl = (dummy ? list : remove_dummies(list))
+    # Enumerate the atoms - by name, not atomic number
     names = atomnames(atl)
+    # Loop through the atoms and
     for (n,atom) in enumerate(atl)
         println(
+            # Print the atom number and type (enumerated)
             io, n, " ", findfirst(isequal(atomname(atom)), names),
+            # Print the Cartesian coordinates
             @sprintf("%f  %f  %f", basis(atl) * atom.pos)
         )
-    end 
-    # Print the atom list
-    println(io, "\nAtoms  # atomic\n")
-    for (n,atom) in enumerate(xtal.gen)
-        println(io, n, " ", )
     end
 end
 
+# Same thing, but with an optional supercell vector
+# (This is automatically converted to a diagonal matrix)
+write_lammps_data(
+    io::IO, 
+    list::AtomList{D},
+    supercell::AbstractVector{<:Integer} = ones(Int, D);
+    kwargs...
+) where D = write_lammps_data(io, list, diagm(supercell); kwargs...)
+
+# Same thing, but with an AbstractCrystal
 write_lammps_data(
     io::IO, 
     xtal::AbstractCrystal{D},
-    supercell::AbstractVector{<:Integer} = ones(Int, D)
-) where D = write_lammps_data(io, xtal, diagm(supercell))
+    supercell;
+    kwargs...
+) where D = write_lammps_data(io, xtal.gen, supercell; kwargs...)
 
-function write_lammps_data(filename::AbstractString, xtal::Crystal{D}; kwargs...) where D
+# Write to a filename
+function write_lammps_data(
+    filename::AbstractString,
+    xtal,
+    supercell = ones(Int, D);
+    kwargs...
+) where D
     open(filename, write=true) do io
-        write_lammps_data(io, xtal, kwargs...)
+        write_lammps_data(io, xtal, supercell; kwargs...)
     end
 end
