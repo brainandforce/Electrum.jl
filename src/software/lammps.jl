ad026e9731dd59284bc506d5c55991db012ea090
Brandon Flores <bsflores@wisc.edu>
2022-08-30 21:16:42 -0500

    Refactored all LAMMPS data writing code

--- src/software/lammps.jl
+++ src/software/lammps.jl
@@ -1,40 +1,33 @@
 """
     write_lammps_data(
         io::IO,
-        xtal::AbstractCrystal{D};
-        supercell = diagm(ones(Int, D))::AbstractVector{<:Integer};
-        dummy::Bool = false 
+        list::AtomList{D}
+        sc::AbstractVecOrMat{<:Integer} = ones(Int,D);
+        dummy::Bool = false
     )
 
 Writes crystal information to a LAMMPS data format that can be used to define a simulation box
-for running a molecular dynamics simulation.
+for running a molecular dynamics simulation. If provided, `sc` can be used to generate a supercell
 
-This function currently only works for 3D systems.
+This function currently only works correctly for 3D systems.
 """
-function write_lammps_data(
-    io::IO,
-    list::AtomList{D},
-    supercell::AbstractMatrix{<:Integer};
-    dummy::Bool=false
-) where D
+function write_lammps_data(io::IO, list::AtomList{D}, dummy::Bool=false) where D
     println(io, "# LAMMPS data file written by Xtal.jl (https://github.com/brainandforce/Xtal.jl)")
     # Get the number of atoms; write the corresponding line
-    natoms = length(list) * convert(Int, abs(det(supercell)))
+    natoms = length(list)
     println(io, natoms, " atoms")
     # Get the number of atom types
     println(io, natomtypes(list), " atom types")
-    # Convert to upper triangular form
-    bnew = triangularize(basis(list), supercell)
     # Now print the new basis vectors
     println(io, @sprintf("0.000000    %f", bnew[1,1]), "     xlo xhi")
     println(io, @sprintf("0.000000    %f", bnew[2,2]), "     ylo yhi")
     println(io, @sprintf("0.000000    %f", bnew[3,3]), "     zlo zhi")
     # Add in tilt factors if needed
     if !isdiag(matrix(bnew))
-        println(io, @sprintf("%f %f %f", bnew[1,2], bnew[1,3], bnew[2,3]), " xy xz yz")
+        println(io, "# Tilt factors:")
+        @printf(io, "%f %f %f xy xz yz", bnew[1,2], bnew[1,3], bnew[2,3])
     end
-    # Get the different atom types
-    # Strip dummy atoms if needed
+    # Get the different atom types; strip dummy atoms if needed
     atl = (dummy ? list : remove_dummies(list))
     # Enumerate the atoms - by name, not atomic number
     names = atomnames(atl)
@@ -50,14 +43,27 @@ function write_lammps_data(
     end
 end
 
-# Same thing, but with an optional supercell vector
-# (This is automatically converted to a diagonal matrix)
-write_lammps_data(
-    io::IO, 
+"""
+    write_lammps_data(
+        io::IO,
+        xtal::AbstractCrystal{D};
+        sc = diagm(ones(Int, D))::AbstractVector{<:Integer};
+        dummy::Bool = false 
+    )
+
+Writes crystal information to a LAMMPS data format that can be used to define a simulation box
+for running a molecular dynamics simulation.
+
+This function currently only works for 3D systems.
+"""
+function write_lammps_data(
+    io::IO,
     list::AtomList{D},
-    supercell::AbstractVector{<:Integer} = ones(Int, D);
+    sc::AbstractVecOrMat{<:Integer};
     kwargs...
-) where D = write_lammps_data(io, list, diagm(supercell); kwargs...)
+) where D
+    write_lammps_data(io, supercell(list, sc); kwargs...)
+end
 
 # Same thing, but with an AbstractCrystal
 write_lammps_data(
@@ -70,7 +76,7 @@ write_lammps_data(
 # Write to a filename
 function write_lammps_data(
     filename::AbstractString,
-    xtal::Union{AbstractCrystal{D},AtomList{D}}, # this is weird...
+    xtal,
     supercell = ones(Int, D);
     kwargs...
 ) where D
