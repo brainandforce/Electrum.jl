a13c24586305ba5736e998ba276f7e16bb322fa9
Brandon Flores <bsflores@wisc.edu>
2022-10-06 13:48:59 -0500

    Updated test to reflect space stripping in XSF key names

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test, Xtal
 xsf = readXSF3D("files/test.xsf")
 
 @testset "File formats" begin
-    @test haskey("this_is_3Dgrid#1", xsf.data) broken=true
-    @test size(xsf["this_is_3Dgrid#1           "]) == (4, 4, 4)
+    @test haskey(xsf.data, "this_is_3Dgrid#1")
+    @test size(xsf["this_is_3Dgrid#1"]) == (4, 4, 4)
 end
