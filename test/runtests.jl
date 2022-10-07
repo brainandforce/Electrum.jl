4153f842e6c5584e3cd6def9c1a05a861917802c
Brandon Flores <bsflores@wisc.edu>
2022-10-07 00:59:58 -0500

    Added tests for `RealBasis`/`ReciprocalBasis` conversion

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
+using LinearAlgebra, StaticArrays
 using Test, Xtal
 
 xsf = readXSF3D("files/test.xsf")
+
+@testset "Basis vectors" begin
+    # Get basis vectors from the XSF file
+    real_basis = basis(xsf.gen)
+    # Check that conversion between real and reciprocal bases is invertible
+    @test (RealBasis(ReciprocalBasis(b)).vs .- b.vs) ≈ 0
+end
 
 @testset "File formats" begin
     @test haskey(xsf.data, "this_is_3Dgrid#1")
