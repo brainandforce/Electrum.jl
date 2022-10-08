4ca9781b391172c694e627fc80593ab898f6af5e
Brandon Flores <bsflores@wisc.edu>
2022-10-08 01:48:14 -0500

    Moved FFT methods into their own file

--- src/data/realspace.jl
+++ src/data/realspace.jl
@@ -227,19 +227,6 @@ function integrate(f::Function, g::RealSpaceDataGrid)
     return sum(f.(grid(g))) * voxelsize(g)
 end
 
-"""
-    fftfreq(g::RealSpaceDataGrid{D,<:Any}) -> Array{NTuple{D,Float64},D}
-
-Returns the discrete Fourier transform frequency bins for a `RealSpaceDataGrid`.
-"""
-function FFTW.fftfreq(g::RealSpaceDataGrid{D,<:Any}) where D
-    return collect(
-        Iterators.product(
-            (fftfreq(size(g)[d], size(g)[d] / lengths(basis(g))[d]) for d in 1:D)...
-        )
-    )
-end
-
 function d_spacing(g::RealSpaceDataGrid, miller::AbstractVector{<:Integer})
     return d_spacing(basis(g), miller)
 end
