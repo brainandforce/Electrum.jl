a4918c46c1e90563a1f80b629250694ef6b26446
Brandon Flores <bsflores@wisc.edu>
2022-10-09 01:50:39 -0500

    Added more precompile directives for file writing functions

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,13 @@
+precompile(RealBasis, (AbstractMatrix{<:Real},))
+precompile(ReciprocalBasis, (AbstractMatrix{<:Real},))
+
+precompile(readXSF, (AbstractString,))
+precompile(writeXSF, (AbstractString, CrystalWithDatasets))
+
 precompile(read_abinit_density, (AbstractString,))
+precompile(read_abinit_potential, (AbstractString,))
+precompile(read_abinit_wavefunction, (AbstractString,))
+
+precompile(readPOSCAR, ())
 precompile(readWAVECAR, ())
 precompile(readDOSCAR, ())
