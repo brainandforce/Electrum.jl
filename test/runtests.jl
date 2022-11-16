ee837be1a646839155292b5cf2b1a3f15cc8d973
Brandon Flores <bsflores@wisc.edu>
2022-11-16 11:47:46 -0600

    Moved supercell checks into their own file

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,72 +18,7 @@ poscar = readPOSCAR("files/")
         @test b ≈ RealBasis(ReciprocalBasis(b))
     end
 
-    @testset "Supercell construction" begin
-        # Coordinates for Laves-MgZn2
-        MgZn2 = AtomList(
-            RealBasis(5.223 * SMatrix{3,3,Float64}(1, 0, 0, -1/2, sqrt(3/4), 0, 0, 0, sqrt(8/3))),
-            [
-                AtomPosition(12, SVector(1/3, 2/3, 1/16)),
-                AtomPosition(12, SVector(1/3, 2/3, 7/16)),
-                AtomPosition(12, SVector(2/3, 1/3, 9/16)),
-                AtomPosition(12, SVector(2/3, 1/3, 15/16)),
-                AtomPosition(30, SVector(0.0, 0.0, 0.0)),
-                AtomPosition(30, SVector(0.0, 0.0, 1/2)),
-                AtomPosition(30, SVector(5/6, 2/3, 1/4)),
-                AtomPosition(30, SVector(5/6, 1/6, 1/4)),
-                AtomPosition(30, SVector(1/3, 1/6, 1/4)),
-                AtomPosition(30, SVector(1/6, 1/3, 3/4)),
-                AtomPosition(30, SVector(1/6, 5/6, 3/4)),
-                AtomPosition(30, SVector(2/3, 5/6, 3/4)),
-                # Two duplicates for good measure!
-                AtomPosition(12, SVector(4/3, 2/3, 1/16)),
-                AtomPosition(30, SVector(0.0, 0.0, 1/2))
-            ]
-        )
-        # The extra Zn atom should be stripped because it's identical to an earlier one
-        @test length(MgZn2) == 13
-        # Build an orthorhombic supercell (treat hexagonal as orthorhombic C)
-        transform = [1 1 0; -1 1 0; 0 0 1]
-        MgZn2_sc = supercell(MgZn2, transform)
-        # Check that the basis is actually orthorhombic
-        @test convert(SMatrix, basis(MgZn2_sc)) ≈ diagm(SVector(9.0465013679, 5.223, 8.5291232843))
-        # Check that the atomic list size scales with the transform matrix
-        @test length(MgZn2_sc)/length(MgZn2) <= abs(det(transform))
-        # Check that the duplicate Mg atom was stripped
-        @test length(MgZn2_sc) == 24
-        # Check each individual atom:
-        #=
-        @testset "MgZn2 supercell construction check" begin
-            # Just work with the coordinates
-            Mg = [coord(a) for a in filter(12, MgZn2_sc)]
-            Zn = [coord(a) for a in filter(30, MgZn2_sc)]
-            @test any(isapprox(SVector(1/2,   1/6,  1/16)), Mg)
-            @test any(isapprox(SVector(1/2,   1/6,  7/16)), Mg)
-            @test any(isapprox(SVector(1/2,   5/6,  9/16)), Mg)
-            @test any(isapprox(SVector(1/2,   5/6, 15/16)), Mg)
-            @test any(isapprox(SVector(  0,   2/3,  1/16)), Mg)
-            @test any(isapprox(SVector(  0,   2/3,  7/16)), Mg)
-            @test any(isapprox(SVector(  0,   1/3,  9/16)), Mg)
-            @test any(isapprox(SVector(  0,   1/3, 15/16)), Mg)
-            @test any(isapprox(SVector(0.0,   0.0,   0.0)), Zn)
-            @test any(isapprox(SVector(  0,     0,   1/2)), Zn)
-            @test any(isapprox(SVector(1/2,   1/2,   0.0)), Zn)
-            @test any(isapprox(SVector(1/2,   1/2,   1/2)), Zn)
-            @test any(isapprox(SVector(  0,   1/6,   1/4)), Zn)
-            @test any(isapprox(SVector(  0,   5/6,   3/4)), Zn)
-            @test any(isapprox(SVector(1/4,  5/12,   1/4)), Zn)
-            @test any(isapprox(SVector(3/4,  5/12,   1/4)), Zn)
-            @test any(isapprox(SVector(1/2,   1/3,   1/4)), Zn)
-            @test any(isapprox(SVector(1/4, 11/12,   1/4)), Zn)
-            @test any(isapprox(SVector(3/4, 11/12,   1/4)), Zn)
-            @test any(isapprox(SVector(1/4,  1/12,   3/4)), Zn)
-            @test any(isapprox(SVector(3/4,  1/12,   3/4)), Zn)
-            @test any(isapprox(SVector(1/2,   1/3,   3/4)), Zn)
-            @test any(isapprox(SVector(1/4,  7/12,   3/4)), Zn)
-            @test any(isapprox(SVector(3/4,  7/12,   3/4)), Zn)
-        end
-        =#
-    end
+    include("supercell.jl")
 
     @testset "RealSpaceDataGrid" begin
         g = xsf["this_is_3Dgrid#1"]
