6dc1fb77bae7f6bd5b752ad7aa4a0e09a59a34d6
Brandon Flores <bsflores@wisc.edu>
2023-03-17 02:06:37 -0500

    Added export for `readCONTCAR()`

--- src/Electrum.jl
+++ src/Electrum.jl
@@ -139,7 +139,8 @@ export read_abinit_density, read_abinit_potential, read_abinit_wavefunction, rea
        read_abinit_POT, read_abinit_anaddb_out, read_abinit_WFK, read_abinit_anaddb_in,
        write_abinit_modes, read_abinit_anaddb_PHDOS
 include("software/vasp.jl")
-export readPOSCAR, writePOSCAR4, readWAVECAR, readDOSCAR, readPROCAR, get_fermi, readKPOINTS
+export readPOSCAR, readCONTCAR, writePOSCAR4, readWAVECAR, readDOSCAR, readPROCAR, get_fermi,
+       readKPOINTS
 include("software/lammps.jl")
 export write_lammps_data
 # Show methods for pretty printing this module's structs
