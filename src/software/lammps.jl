2e91145604c4cba3d9eadf1712e735dade4c4ea6
Brandon Flores <bsflores@wisc.edu>
2023-04-03 15:42:56 -0500

    Added functions for reading LAMMPS atomic data

--- src/software/lammps.jl
+++ src/software/lammps.jl
@@ -1,4 +1,65 @@
 """
+    read_lammps_data(io::IO; atoms::AbstractVector = NamedAtom[]) -> PeriodicAtomList{3}
+
+Reads a LAMMPS data file containing atomic coordinates. Currently, this function only supports 3D
+crystal data.
+"""
+function read_lammps_data(io::IO; atom_types::AbstractVector = NamedAtom[]) 
+    natom = 0
+    ntypat = length(atom_types)
+    basis_M = zeros(MMatrix{3,3,Float64})
+    while !eof(io)
+        # Get rid of any comment characters
+        ln = strip(first(split(readline(io), '#')))
+        # Get the number of atoms
+        if isempty(ln)
+            continue
+        elseif ln == "Atoms"
+            break
+        elseif occursin(" atoms", ln)
+            natom = parse(Int, first(split(ln)))
+        elseif occursin(" atom types", ln)
+            ntypat = parse(Int, first(split(ln)))
+        elseif occursin(r"xlo.+xhi", ln)
+            basis_M[1,1] = parse(Float64, split(ln)[2]) - parse(Float64, split(ln)[1])
+        elseif occursin(r"ylo.+yhi", ln)
+            basis_M[2,2] = parse(Float64, split(ln)[2]) - parse(Float64, split(ln)[1])
+        elseif occursin(r"zlo.+zhi", ln)
+            basis_M[3,3] = parse(Float64, split(ln)[2]) - parse(Float64, split(ln)[1])
+        # Match these in any order
+        elseif all(occursin(t, ln) for t in ["xy", "xz", "yz"])
+            tilt_tokens = filter(s -> (s in ["xy", "xz", "yz"]), split(ln))
+            indices = [
+                Tuple(parse(Int, c) for c in s)
+                for s in replace.(tilt_tokens, "x" => "1", "y" => "2", "z" => "3")
+            ]
+            for (i,s) in zip(indices, split(ln)[1:3])
+                basis_M[i...] = parse.(Float64, s)
+            end
+        end
+    end
+    atom_vec = Vector{FractionalAtomPosition{3}}(undef, natom)
+    ind = 1
+    while ind <= natom && !eof(io)
+        # Get rid of any comment characters
+        ln = strip(first(split(readline(io), '#')))
+        isempty(ln) && continue
+        # Convert the Cartesian coordinate to fractional
+        coord_cart = SVector{3,Float64}(parse(Float64, s) for s in split(ln)[3:end])
+        coord_frac = basis_M \ coord_cart
+        atom_vec[ind] = if !isempty(atom_types)
+            FractionalAtomPosition(atom_types[parse(Int, split(ln)[2])], coord_frac)
+        else
+            FractionalAtomPosition(0, coord_frac)
+        end
+        ind += 1
+    end
+    return PeriodicAtomList(RealBasis(basis_M), atom_vec)
+end
+
+read_lammps_data(filename; kwargs...) = open(io -> read_lammps_data(io; kwargs...), filename)
+
+"""
     write_lammps_data(io::IO, list::PeriodicAtomList, [transform]; dummy::Bool = false)
 
 Writes crystal information to a LAMMPS data format that can be used to define a simulation box for
@@ -46,7 +107,7 @@ function write_lammps_data(io::IO, list::PeriodicAtomList{D}; dummy::Bool=false)
         for m in 1:D
             @printf(io, "% 16.10f", (basis(atl) * displacement(atom))[m])
         end
-        print(io, "\n")
+        println(io)
     end
 end
 
