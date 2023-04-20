370311aac59a3841c923fd0fc565a63681fb2f24
Brandon Flores <bsflores@wisc.edu>
2023-04-20 17:32:24 -0500

    Added tests for FFT indexing tools

--- test/datagrids.jl
+++ test/datagrids.jl
@@ -7,9 +7,18 @@
     @test g[1,1,1] === 1.732
     @test g[1,2,3] === 3.742
 end
+
 @testset "Fourier transforms" begin
     g = xsf["this_is_3Dgrid#1"]
     hkl = fft(g)
     # Check that the Fourier transform and its inverse undo each other
     @test g ≈ ifft(fft(g))
+    # Test FFT indexing mechanics
+    @test collect(FFTLength(4)) == [0, 1, -2, -1]
+    @test collect(FFTLength(5)) == [0, 1, 2, -2, -1]
+    @test sort(FFTLength(4)) == -2:1
+    @test sort(FFTLength(5)) == -2:2
+    @test collect(FFTBins(4)) == [CartesianIndex(x) for x in [0, 1, -2, -1]]
+    @test collect(FFTBins(3,3)) == [CartesianIndex(x,y) for x in [0, 1, -1], y in [0, 1, -1]]
+    @test FFTBins(CartesianIndices(FFTBins(3,3))) == FFTBins(3,3)
 end
