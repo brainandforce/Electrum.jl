3f742df6e981a43f74e612e494f0fceba44c97bd
Brandon Flores <bsflores@wisc.edu>
2023-04-20 13:22:15 -0500

    Added more tests for internals

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,6 +30,18 @@ lammps = read_lammps_data("files/lammps.data", atom_types = [14, 77])
     include("filetypes.jl")
     include("kpoints.jl")
     @testset "Miscellaneous" begin
+        # Linear independence
+        @test Electrum.is_linearly_independent([1, 1], [2, 2]) === false
+        # Conversion of scalars/vectors to transformation matrices
+        import Electrum.convert_to_transform
+        @test convert_to_transform(2, Val{3}()) == diagm(fill(2, SVector{3}))
+        @test convert_to_transform(2, 3) == diagm(fill(2, 3))
+        @test convert_to_transform(2*LinearAlgebra.I, 3) == diagm(fill(2, 3))
+        @test convert_to_transform([1,2,3]) == diagm([1,2,3])
+        @test convert_to_transform(SVector{3}(1,2,3)) == diagm(SVector{3}(1,2,3))
+        @test convert_to_transform([1,2,3], Val{3}()) == diagm(SVector{3}(1,2,3))
+        # Arguments not convertible to integers should fail
+        @test_throws InexactError convert_to_transform(1.5, 2)
         # Data space traits
         @test Electrum.data_space(xsf["this_is_3Dgrid#1"]) === Electrum.ByRealSpace{3}()
         @test Electrum.data_space(v80_wfk["wavefunction"]) === Electrum.ByReciprocalSpace{3}()
