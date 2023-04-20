da398758649de1fb51317e342075689b18762dad
Brandon Flores <bsflores@wisc.edu>
2023-04-20 12:57:35 -0500

    Shortened definitions for `Electrum.is_linearly_independent()`

--- src/math.jl
+++ src/math.jl
@@ -1,20 +1,13 @@
 """
-    Electrum._is_linearly_independent(vecs::AbstractMatrix{<:Number}) -> Bool
-    Electrum._is_linearly_independent(vecs::AbstractVector{<:Number}...) -> Bool
+    Electrum._is_linearly_independent(M::AbstractMatrix) -> Bool
+    Electrum._is_linearly_independent(vecs::AbstractVector...) -> Bool
 
 Determines whether a set of vectors is linearly independent.
 
 This function always returns `false` if the first dimension of the matrix is less than the second.
 """
-function is_linearly_independent(vecs::AbstractMatrix{<:Number})
-    size(vecs)[1] < size(vecs)[2] || return false
-    return LinearAlgebra.rank(vecs) == minimum(size(vecs))
-end
-
-# Same thing but for sets of vectors
-function is_linearly_independent(vecs::AbstractVector{<:Number}...)
-    return is_linearly_independent(hcat(vecs...))
-end
+is_linearly_independent(M::AbstractMatrix) = !isless(size(M)...) && rank(M) == minimum(size(M))
+is_linearly_independent(vecs::AbstractVector...) = is_linearly_independent(hcat(vecs...))
 
 """
     Electrum.reinterpret_index(sz::NTuple{D,<:Integer}, inds::Tuple) -> Tuple
