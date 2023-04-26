7d72e642244dbbd57f8082a420eb084b01e03446
Brandon Flores <bsflores@wisc.edu>
2023-04-25 19:07:17 -0500

    Added tests for `KPointMesh`

--- test/kpoints.jl
+++ test/kpoints.jl
@@ -18,3 +18,28 @@
     @test klist[1:3] == klist2
     @test sum(klist.weights) ≈ 1
 end
+
+@testset "k-point mesh" begin
+    kptmesh = KPointMesh(
+        [
+            KPoint(0.0, 0.0, 0.0, weight = 1),
+            KPoint(0.2, 0.0, 0.0, weight = 6),
+            KPoint(0.4, 0.0, 0.0, weight = 6),
+            KPoint(0.2, 0.2, 0.0, weight = 6),
+            KPoint(0.4, 0.2, 0.0, weight = 6),
+            KPoint(0.0, 0.0, 1/3, weight = 2),
+            KPoint(0.2, 0.0, 1/3, weight = 12),
+            KPoint(0.4, 0.0, 1/3, weight = 12),
+            KPoint(0.2, 0.2, 1/3, weight = 12),
+            KPoint(0.4, 0.2, 1/3, weight = 12)
+        ],
+        diagm(SVector{3}(5,5,3))
+    )
+    # @test nkpt(kptmesh) == 10
+    @test size(kptmesh) == (10,)
+    @test axes(kptmesh) == (Base.OneTo(10),)
+    @test sum(weight.(kptmesh)) == det(kptmesh.grid) == 75
+    @test kptmesh[end] == KPoint(0.4, 0.2, 1/3, weight = 12)
+    @test allunique(kptmesh)
+    @test kptmesh[1:10] == kptmesh
+end
