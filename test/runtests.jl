79ff839cb9d8d31834e1e4e74b86d0962102ffad
Brandon Flores <bsflores@wisc.edu>
2023-04-26 02:22:36 -0500

    Updated tests to reflect the `read_abinit_wavefunction_new()`

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ Aqua.test_all(Electrum; project_toml_formatting=false)
 
 xsf = readXSF3D("files/test.xsf")
 v80_den = read_abinit_density("files/Sc_eq_o_DEN")
-v80_wfk = read_abinit_wavefunction("files/Sc_eq_o_WFK")
+v80_wfk = Electrum.read_abinit_wavefunction_new("files/Sc_eq_o_WFK")
 # These two have the same data. Well...almost...
 poscar = readPOSCAR("files/POSCAR")
 lammps = read_lammps_data("files/lammps.data", atom_types = [14, 77])
