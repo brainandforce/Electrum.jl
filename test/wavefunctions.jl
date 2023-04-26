4d63328b0185d910a0d111d029a507b74e98e75b
Brandon Flores <bsflores@wisc.edu>
2023-04-26 02:39:09 -0500

    Converted wavefunction reading functions to using `PlanewaveWavefunction`

--- test/wavefunctions.jl
+++ test/wavefunctions.jl
@@ -1,15 +1,13 @@
-wf = Electrum.readWAVECAR_new("files/WAVECAR")
-
 @testset "Wavefunctions" begin
     i = PlanewaveIndex(1, 2, 3, 4, -5, 6)
-    @test size(wf) === (1, 6, 12, 13, 13, 13)
-    @test axes(wf) === (Base.OneTo(1), Base.OneTo(6), Base.OneTo(12), -6:6, -6:6, -6:6)
-    wf[1, 2, 3, 4, -5, 6] = 420
+    @test size(wavecar) === (1, 6, 12, 13, 13, 13)
+    @test axes(wavecar) === (Base.OneTo(1), Base.OneTo(6), Base.OneTo(12), -6:6, -6:6, -6:6)
+    wavecar[1, 2, 3, 4, -5, 6] = 420
     # Test that getindex and setindex! indices are the same
-    @test wf[1,2,3,4,-5,6] == 420
+    @test wavecar[1,2,3,4,-5,6] == 420
     # Periodic indexing
-    @test wf[1, 2, 3, 13, -13, 0] === wf[1, 2, 3, 0, 0, 0]
-    @test_throws BoundsError wf[1, 7, 12, 0, 0, 0]
-    @test wf[:,:,:] == wf[1, 1:6, 1:12] == wf
-    @test wf[i] == wf[CartesianIndex(i)]
+    @test wavecar[1, 2, 3, 13, -13, 0] === wavecar[1, 2, 3, 0, 0, 0]
+    @test_throws BoundsError wavecar[1, 7, 12, 0, 0, 0]
+    @test wavecar[:,:,:] == wavecar[1, 1:6, 1:12] == wavecar
+    @test wavecar[i] == wavecar[CartesianIndex(i)]
 end
