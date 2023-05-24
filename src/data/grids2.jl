9833cdc37035608429c4afbf8f6001ba392ef9bb
Brandon Flores <bsflores@wisc.edu>
2023-05-24 17:33:10 -0500

    Defined `fftfreq(::DataGrid)`

--- src/data/grids2.jl
+++ src/data/grids2.jl
@@ -291,6 +291,29 @@ The inverse FFT is normalized so that `ifft(fft(g)) ≈ g` (to within floating p
 """
 FFTW.ifft(g::DataGrid) = DataGrid(bfft(g.data) * fftvol(g), inv(basis(g)))
 
+"""
+    fftfreq(g::DataGrid{D}) -> Array{SVector{D,Float64},D}
+
+Returns the Fourier transform frequency bins for an `DataGrid`.
+
+For real space data, the frequency bins will be angular wavenumbers, matching the 2π factors that
+are introduced when transforming between a `RealBasis` and a `ReciprocalBasis`. The convention used
+by `FFTW.fftfreq()` is also used: frequency bins at or above the Nyquist frequency will be negative.
+
+For reciprocal space data, the frequencies are binned with the assumption that the lattice vectors
+are given in angular wavenumbers, and they represent real space coordinates. The Nyquist frequency
+convention is *not* used, so all elements will have positive indices.
+"""
+function FFTW.fftfreq(g::DataGrid, ::ByRealSpace)
+    return map(i -> SVector(2π .* Tuple(i) ./ size(g)), FFTBins(g))
+end
+
+function FFTW.fftfreq(g::DataGrid, ::ByReciprocalSpace)
+    return map(i -> SVector(Tuple(i) .* size(g) ./ 2π), CartesianIndices(g))
+end
+
+FFTW.fftfreq(g::DataGrid) = fftfreq(g, DataSpace(g))
+
 #---Operations specific to `RealDataGrid`----------------------------------------------------------#
 """
     integrate([f::Function = identity], g::RealDataGrid{D,T}) -> T
