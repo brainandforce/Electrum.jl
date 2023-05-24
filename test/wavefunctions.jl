64f79d4820bda89bdc0de59b9df413a543e37881
Brandon Flores <bsflores@wisc.edu>
2023-05-24 17:55:34 -0500

    Added extra tests for wavefunction indexing

--- test/wavefunctions.jl
+++ test/wavefunctions.jl
@@ -3,6 +3,11 @@
     @test size(wavecar) === (1, 6, 12, 13, 13, 13)
     @test axes(wavecar) === (Base.OneTo(1), Base.OneTo(6), Base.OneTo(12), -6:6, -6:6, -6:6)
     wavecar[1, 2, 3, 4, -5, 6] = 420
+    @test wavecar[1, 1, 1, 4, 0, 0] == -4.900393f-5 + 1.3592809f-5im
+    @test iszero(wavecar[1, 1, 1, 5, 0, 0])
+    @test iszero(wavecar[1, 1, 1, 5, 0, 0])
+    @test wavecar[1, 1, 1, -4, 0, 0] == -4.900393f-5 + 1.3592809f-5im
+    @test wavecar[1, 6, 12, -1, -1, -1] == -0.070487864f0 - 0.1318744f0im
     # Test that getindex and setindex! indices are the same
     @test wavecar[1,2,3,4,-5,6] == 420
     # Periodic indexing
