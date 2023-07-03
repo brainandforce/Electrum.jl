213e4f234366574769a899c3093e154889144ad4
Brandon Flores <bsflores@wisc.edu>
2023-07-03 16:36:49 -0500

    Restructured TOML writing to use conversion function

--- src/software/toml.jl
+++ src/software/toml.jl
@@ -1,36 +1,45 @@
-function TOML.print(io::IO, b::AbstractBasis)
-    println(io, "space = \"re", "cipro"^(DataSpace(b) isa ByReciprocalSpace), "al\"")
-    println(io, "vectors = [", )
-    for v in b
-        print(io, " "^4, "[")
-        join(io, v, ", ")
-        println(io, "],")
-    end
-    println(io, "]")
+function to_toml_data(b::AbstractBasis)
+    return Dict{String,Any}(
+        "realspace" => !(DataSpace(b) isa ByReciprocalSpace),
+        "dimension" => size(b,1),
+        "vectors" => b.vectors
+    )
 end
 
-function TOML.print(io::IO, p::PeriodicAtomList)
-    println(io, "[basis]")
-    TOML.print(io, basis(p))
-    for atom in p
-        println(io, "\n[[atoms]]")
-        println(io, "name = ", name(atom))
-        println(io, "number = ", atomic_number(atom))
-        println(io, "coordinate = ", displacement(atom))
-    end
+to_toml_data(a::NamedAtom) = Dict{String,Any}("name" => name(a), "number" => atomic_number(a))
+
+function to_toml_data(p::CartesianAtomPosition)
+    data = to_toml_data(NamedAtom(p))
+    data["cartesian"] = displacement(p)
+    return data
 end
 
-function TOML.print(io::IO, xtal::Crystal)
-    # Transform goes in the top-level table
-    println(io, "transform = [")
-    for v in eachcol(xtal.transform)
-        print(io, " "^4, "[")
-        join(io, v, ", ")
-        println(io, "],")
-    end
-    println(io, "]\n")
-    TOML.print(io, xtal.atoms)
-    println(io, "\n[space_group]")
-    println(io, "number = ", xtal.sgno)
-    println(io, "origin = ", xtal.sgorig)
+function to_toml_data(p::FractionalAtomPosition)
+    data = to_toml_data(NamedAtom(p))
+    data["fractional"] = displacement(p)
+    return data
 end
+
+function to_toml_data(p::FractionalAtomPosition, b::AbstractBasis)
+    data = to_toml_data(p)
+    data["cartesian"] = RealBasis(b) * displacement(p)
+    return data
+end
+
+function to_toml_data(l::AtomList)
+    return Dict{String,Any}(to_toml_data.(l))
+end
+
+function to_toml_data(l::PeriodicAtomList)
+    return Dict{String,Any}("basis" => to_toml_data(basis(l)), "atoms" => to_toml_data.(l))
+end
+
+function to_toml_data(x::Crystal)
+    data = to_toml_data(x.atoms)
+    data["space_group"] = Dict{String,Any}("number" => x.sgno, "origin" => x.sgorig)
+    data["transform"] = eachcol(x.transform)
+    return data
+end
+
+TOML.print(io::IO, x::AbstractAtomList; kwargs...) = TOML.print(io, to_toml_data(x); kwargs...)
+TOML.print(io::IO, x::Crystal; kwargs...) = TOML.print(io, to_toml_data(x); kwargs...)
