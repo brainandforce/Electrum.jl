5d328fbce5853048053f0c0790290c08fda60bce
Brandon Flores <bsflores@wisc.edu>
2023-10-20 10:59:45 -0500

    Added tests for convert methods

--- test/atoms.jl
+++ test/atoms.jl
@@ -6,4 +6,9 @@
     @test isdummy(a) === false
     @test NamedAtom(17) === NamedAtom("Cl", 17)
     @test isdummy(NamedAtom("test")) === true
+    @test convert(String, a) == "Cl1"
+    @test convert(Int, a) === 17
+    @test convert(NamedAtom, 69) == NamedAtom("Tm")
+    @test convert(NamedAtom, "Tm") == NamedAtom(69)
+    @test convert(NamedAtom, "Tm420") == NamedAtom("Tm420", 69)
 end
