56f54d5d64399c2ff52f67e596a0fb27ad4795aa
Brandon Flores <bsflores@wisc.edu>
2023-11-09 14:30:47 -0600

    Fixed bad names in tests

--- test/wavefunctions.jl
+++ test/wavefunctions.jl
@@ -18,15 +18,15 @@
     @test wavecar[1, 2, 3] isa ReciprocalDataGrid{3}
     @test vec(wavecar[1, 2, 3].data) == vec(wavecar.data[:, 3, 2, 1])
     # Energies and occupancies
-    @test wavecar.energies == energies(EnergyOccupancy(wavecar))
-    @test wavecar.occupancies == occupancies(EnergyOccupancy(wavecar))
+    @test wavecar.energies == energies(EnergiesOccupancies(wavecar))
+    @test wavecar.occupancies == occupancies(EnergiesOccupancies(wavecar))
     @test min_energy(wavecar) == minimum(wavecar.energies)
     @test max_energy(wavecar) == maximum(wavecar.energies)
-    @test min_energy(wavecar) == min_energy(EnergyOccupancy(wavecar))
-    @test max_energy(wavecar) == max_energy(EnergyOccupancy(wavecar))
+    @test min_energy(wavecar) == min_energy(EnergiesOccupancies(wavecar))
+    @test max_energy(wavecar) == max_energy(EnergiesOccupancies(wavecar))
     @test min_occupancy(wavecar) == minimum(wavecar.occupancies)
     @test max_occupancy(wavecar) == maximum(wavecar.occupancies)
-    @test min_occupancy(wavecar) == min_occupancy(EnergyOccupancy(wavecar))
-    @test max_occupancy(wavecar) == max_occupancy(EnergyOccupancy(wavecar))
+    @test min_occupancy(wavecar) == min_occupancy(EnergiesOccupancies(wavecar))
+    @test max_occupancy(wavecar) == max_occupancy(EnergiesOccupancies(wavecar))
     @test min_energy(wavecar) <= fermi(wavecar) <= max_energy(wavecar)
 end
