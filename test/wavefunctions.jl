082c028109ac87075367d0c40ade68b35c4ca7b6
Brandon Flores <bsflores@wisc.edu>
2023-11-09 14:13:55 -0600

    Added tests for new methods

--- test/wavefunctions.jl
+++ test/wavefunctions.jl
@@ -17,7 +17,16 @@
     @test wavecar[i] == wavecar[CartesianIndex(i)]
     @test wavecar[1, 2, 3] isa ReciprocalDataGrid{3}
     @test vec(wavecar[1, 2, 3].data) == vec(wavecar.data[:, 3, 2, 1])
+    # Energies and occupancies
+    @test wavecar.energies == energies(EnergyOccupancy(wavecar))
+    @test wavecar.occupancies == occupancies(EnergyOccupancy(wavecar))
     @test min_energy(wavecar) == minimum(wavecar.energies)
     @test max_energy(wavecar) == maximum(wavecar.energies)
-    @test min_energy(wavecar) < fermi(wavecar) < max_energy(wavecar)
+    @test min_energy(wavecar) == min_energy(EnergyOccupancy(wavecar))
+    @test max_energy(wavecar) == max_energy(EnergyOccupancy(wavecar))
+    @test min_occupancy(wavecar) == minimum(wavecar.occupancies)
+    @test max_occupancy(wavecar) == maximum(wavecar.occupancies)
+    @test min_occupancy(wavecar) == min_occupancy(EnergyOccupancy(wavecar))
+    @test max_occupancy(wavecar) == max_occupancy(EnergyOccupancy(wavecar))
+    @test min_energy(wavecar) <= fermi(wavecar) <= max_energy(wavecar)
 end
