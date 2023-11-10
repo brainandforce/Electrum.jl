75cef2c95e5adc5f31cb2c647b67c9e3b61a5408
Brandon Flores <bsflores@wisc.edu>
2023-11-10 13:19:16 -0600

    Fixed missing export of `nspin`

--- src/Electrum.jl
+++ src/Electrum.jl
@@ -142,7 +142,7 @@ export shift, fft, ifft, fftfreq, voxelsize, integrate, partial_derivative, cell
 # Planewave wavefunctions
 include("data/wavefunctions.jl")
 export PlanewaveIndex, PlanewaveWavefunction
-export nonzero_g_indices, nonzero_g_vectors
+export nspin, nband, nonzero_g_indices, nonzero_g_vectors
 # Band structures
 include("data/bands.jl")
 export BandAtKPoint, BandStructure
