419909702672537366fbac4b22bd95787b629e55
Brandon Flores <bsflores@wisc.edu>
2023-11-16 15:19:34 -0600

    Fixed failed test (missing sign)

--- test/spin.jl
+++ test/spin.jl
@@ -1,6 +1,6 @@
 @testset "Spin" begin
     @test all(last(Multiplicity(n)) == (n - 1)//2 for n in 1:100)
-    @test all(Multiplicity(n)[2] == (n - 1)//2 + 1 for n in 2:100)
+    @test all(Multiplicity(n)[2] == -(n - 1)//2 + 1 for n in 2:100)
     @test all(iszero(last(Multiplicity(n)) + first(Multiplicity(n))) for n in 1:100)
     @test all(Multiplicity(n) == UnitRange(Multiplicity(n)) for n in 1:100)
     @test all(
