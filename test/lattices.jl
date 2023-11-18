4e2b2f41f09b50e4287822b32afad79fe9258b51
Brandon Flores <bsflores@wisc.edu>
2023-11-18 14:48:55 -0600

    Added tests for `eachvertex`

--- test/lattices.jl
+++ test/lattices.jl
@@ -17,4 +17,19 @@
     @test promote_type(RealBasis{3,Int64}, ReciprocalBasis{3,Float32}) === SMatrix{3,3,Float32,9}
     # Metrics
     @test all(x == Electrum.ANG2BOHR for x in lengths(b))
+    # eachvertex iterator
+    @test isequal(
+        Electrum._gen_vertices(reshape(1:69, 23, 3), 1:420),
+        Electrum._gen_vertices(1:69, reshape(1:420, 3, 35, 4))
+    )
+    @test collect(eachvertex([6 0; 0 9])) == vec(collect(eachvertex(SMatrix{2,2}(6, 0, 0, 1))))
+    result = SVector{3,Float64}[
+        [0, 0, 0], [1, 0, 0], [0, 2, 0], [1, 2, 0],
+        [0, 0, 3], [1, 0, 3], [0, 2, 3], [1, 2, 3]
+    ]
+    @test all(isequal.(eachvertex(diagm([1, 2, 3])), result))
+    @test isequal(
+        collect(eachvertex(SMatrix{3,3}(diagm([1, 2, 3])))),
+        reshape(result, 2, 2, 2)
+    )
 end
